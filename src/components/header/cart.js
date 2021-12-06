54c079c5ac87d4768099f73195db13a007363dd2
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2021-12-06 09:33:59 +0500

    Removed coments from Cart.js

--- src/components/header/cart.js
+++ src/components/header/cart.js
@@ -1,41 +1,17 @@
 import React from "react";
-// import {useDispatch} from 'react-redux'
 import Badge from "@material-ui/core/Badge";
 import { useSelector } from "react-redux";
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
-// import ButtonGroup from "@material-ui/core/ButtonGroup";
-// import Button from "@material-ui/core/Button";
-// import AddIcon from "@material-ui/icons/Add";
-// import RemoveIcon from "@material-ui/icons/Remove";
-// import allActions from '../../redux/action'
 
-  
 export default function Cart() {
   const cartReducer = useSelector(state => state.cartReducer)
-  // const [itemCount, setItemCount] = React.useState(0);
-  // const dispatch = useDispatch()
-  
+
   return (
-    <div style={{  display: "block", padding: 20 , color:'white' }}>
+    <div style={{ display: "block", padding: 20, color: 'white' }}>
       <div>
         <Badge color="secondary" badgeContent={cartReducer}>
           <ShoppingCartIcon />{" "}
         </Badge>
-        {/* <ButtonGroup>
-          <Button
-            onClick={()=>dispatch(allActions.cartAction.decrement())}
-          >
-            {" "}
-            <RemoveIcon fontSize="small" />
-          </Button>
-          <Button
-           
-              onClick={()=>dispatch(allActions.cartAction.increment())}
-          >
-            {" "}
-            <AddIcon fontSize="small" />
-          </Button>
-        </ButtonGroup> */}
       </div>
     </div>
   );
